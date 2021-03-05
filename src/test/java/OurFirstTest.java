39d3c40f91f50db8fc741f3a90dee4cce825cff9
iaribko <80049211+iaribko@users.noreply.github.com>
2021-03-05 16:49:38 +0200

    Initial commit

---
+++ src/test/java/OurFirstTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class OurFirstTest {
+    @Test
+    public void firsttest() {
+        System.out.println("Hello world !");
+        System.out.println("Hi 90");
+    }
+}
---
+++ src/test/java/OurSecondTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+public class OurSecondTest {
+    @Test
+    public void secondTest() {
+ //             int a = 10;
+ //            int b = 5;
+
+ //        int sumAB = sumDwoDigits(10, 5);
+
+ //       System.out.println("Sum is: " + sumDwoDigits(10, 5));
+
+        sumDwoDigits(10, 5);
+        sumDwoDigits(100, 33);
+//        String c = "10";
+//        String d = "5";
+
+//             System.out.println("First variable is: " + a + " second is: " + b);
+//
+//              System.out.println(a + b);
+
+//            int e = 100;
+//            int f = 33;
+
+  //      int sumEF = sumDwoDigits(100, 33);
+ //       System.out.println("Sum is: " + sumDwoDigits(100, 33));
+
+//        System.out.println(e + f);
+    }
+
+    private void sumDwoDigits(int a, int b) {
+ //       int sum = a + b;
+        System.out.println("Sum is: " + (a + b));
+//        return sum;
+    }
+}
